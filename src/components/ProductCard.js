2478157fa1024b61a6b0ba60c64c9e3127889967
Ishnoor04 <ishnoorsinghsethi@gmail.com>
2024-08-18 19:10:45 +0530

    product page addToCart fixes

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,24 +1,25 @@
 import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { useSelector, useDispatch } from "react-redux";
-import { addToCart, updateCart } from "@/features/cartSlice";
+import { addToCart, fetchData, updateCart } from "@/features/cartSlice";
 import Ratings from "@/utils/Ratings";
 import { products } from "@/products";
 import { store } from "@/app/store";
 import { handleCurrency } from "@/utils/HandleCurrency";
 import { fetchUser } from "@/features/authSlice";
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ProductCard = ({ res }) => {
   const { items, cart } = useSelector((state) => state.allCart);
   const { user } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
-  useEffect(()=>{
-    dispatch(fetchUser())
-  },[])
+  
+  useEffect(() => {
+    dispatch(fetchUser());
+  }, []);
   const cardRefs = useRef([]);
-  const handleAddToCart = (product, index) => {
+  const handleAddToCart = async (product, index) => {
     if (product.quantity >= product.inStock) {
       alert("Maximum limit reached");
       return;
@@ -66,90 +67,96 @@ const ProductCard = ({ res }) => {
 
       // Add product to cart
 
-      dispatch(addToCart(product));
+       dispatch(addToCart(product));
 
       setTimeout(() => {
         const updatedCart = store.getState().allCart.cart; // Get updated state
         let updatedItem = updatedCart.find((item) => item.id === product.id);
         dispatch(updateCart({ item: updatedItem, remove: false }));
       }, 0);
+
+ 
     }
   };
 
   return (
     <>
       <ToastContainer />
-    <div className="pt-24">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-        {items &&
-          items.map((product, index) => (
-            <div
-              key={product.id}
-              className="max-w-[400px] mx-auto border p-4"
-              ref={(el) => (cardRefs.current[index] = el)}
-            >
-              <div className="w-full max-w-md aspect-square">
-                <img
-                  src={product.image}
-                  alt={product.title}
-                  className="w-full h-full rounded-xl"
-                />
-              </div>
-              <div className="mt-5 flex items-center justify-between">
-                <div>
-                  <h6 className="font-medium text-xl leading-8 text-black mb-2">
-                    {product.title}
-                  </h6>
-                  <h6 className="font-semibold text-xl leading-8 text-indigo-600">
-                    {handleCurrency(product.price)}
-                  </h6>
-                  <div className="flex flex-row gap-2 items-center">
-                    <Ratings rating={product.rating.rate} />
-                    <p className="text-gray-500">({product.rating.count})</p>
-                  </div>
-                  <p className="text-gray-500">{product.inStock} - In Stock</p>
+      <div className="pt-24">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+          {items &&
+            items.map((product, index) => (
+              <div
+                key={product.id}
+                className="max-w-[400px] mx-auto border p-4"
+                ref={(el) => (cardRefs.current[index] = el)}
+              >
+                <div className="w-full max-w-md aspect-square">
+                  <img
+                    src={product.image}
+                    alt={product.title}
+                    className="w-full h-full rounded-xl"
+                  />
                 </div>
-                <button
-                  onClick={() => {
-                    console.log(product.quantity, product.inStock);
-                    if (product.quantity >= product.inStock) {
-                      toast.error("Maximum quantity reached", {
-                        position: "top-right",
-                        autoClose: 3000,
-                        hideProgressBar: false,
-                        closeOnClick: true,
-                        pauseOnHover: true,
-                        draggable: true,
-                        progress: undefined,
-                        theme: "colored",
-                      });
-                    } else {
-                      handleAddToCart(product, index);
-                    }
-                  }}
-                  className="p-2 min-[400px]:p-4 rounded-full bg-white border border-gray-300 flex items-center justify-center group shadow-sm shadow-transparent transition-all duration-500 hover:shadow-gray-200 hover:border-blue-400 hover:bg-blue-50"
-                >
-                  <svg
-                    className="stroke-gray-900 transition-all duration-500 group-hover:stroke-black"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="26"
-                    height="26"
-                    viewBox="0 0 26 26"
-                    fill="none"
+                <div className="mt-5 flex items-center justify-between">
+                  <div>
+                    <h6 className="font-medium text-xl leading-8 text-black mb-2">
+                      {product.title}
+                    </h6>
+                    <h6 className="font-semibold text-xl leading-8 text-indigo-600">
+                      {handleCurrency(product.price)}
+                    </h6>
+                    <div className="flex flex-row gap-2 items-center">
+                      <Ratings rating={product.rating.rate} />
+                      <p className="text-gray-500">({product.rating.count})</p>
+                    </div>
+                    <p className="text-gray-500">
+                      {product.inStock} - In Stock
+                    </p>
+                  </div>
+                  <button
+                    onClick={() => {
+                      console.log(product.quantity, product.inStock);
+                      const updatedCart = store.getState().allCart.cart; // Get updated state
+                      let updatedItem = updatedCart.find((item) => item.id === product.id);
+                      if (updatedItem.quantity >= product.inStock) {
+                        toast.error("Maximum quantity reached", {
+                          position: "top-right",
+                          autoClose: 3000,
+                          hideProgressBar: false,
+                          closeOnClick: true,
+                          pauseOnHover: true,
+                          draggable: true,
+                          progress: undefined,
+                          theme: "colored",
+                        });
+                      } else {
+                        handleAddToCart(product, index);
+                      }
+                    }}
+                    className="p-2 min-[400px]:p-4 rounded-full bg-white border border-gray-300 flex items-center justify-center group shadow-sm shadow-transparent transition-all duration-500 hover:shadow-gray-200 hover:border-blue-400 hover:bg-blue-50"
                   >
-                    <path
-                      d="M12.6892 21.125C12.6892 22.0225 11.9409 22.75 11.0177 22.75C10.0946 22.75 9.34632 22.0225 9.34632 21.125M19.3749 21.125C19.3749 22.0225 18.6266 22.75 17.7035 22.75C16.7804 22.75 16.032 22.0225 16.032 21.125M4.88917 6.5L6.4566 14.88C6.77298 16.5715 6.93117 17.4173 7.53301 17.917C8.13484 18.4167 8.99525 18.4167 10.7161 18.4167H18.0056C19.7266 18.4167 20.587 18.4167 21.1889 17.9169C21.7907 17.4172 21.9489 16.5714 22.2652 14.8798L22.8728 11.6298C23.3172 9.25332 23.5394 8.06508 22.8896 7.28254C22.2398 6.5 21.031 6.5 18.6133 6.5H4.88917ZM4.88917 6.5L4.33203 3.25"
-                      stroke=""
-                      stroke-width="1.6"
-                      stroke-linecap="round"
-                    />
-                  </svg>
-                </button>
+                    <svg
+                      className="stroke-gray-900 transition-all duration-500 group-hover:stroke-black"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="26"
+                      height="26"
+                      viewBox="0 0 26 26"
+                      fill="none"
+                    >
+                      <path
+                        d="M12.6892 21.125C12.6892 22.0225 11.9409 22.75 11.0177 22.75C10.0946 22.75 9.34632 22.0225 9.34632 21.125M19.3749 21.125C19.3749 22.0225 18.6266 22.75 17.7035 22.75C16.7804 22.75 16.032 22.0225 16.032 21.125M4.88917 6.5L6.4566 14.88C6.77298 16.5715 6.93117 17.4173 7.53301 17.917C8.13484 18.4167 8.99525 18.4167 10.7161 18.4167H18.0056C19.7266 18.4167 20.587 18.4167 21.1889 17.9169C21.7907 17.4172 21.9489 16.5714 22.2652 14.8798L22.8728 11.6298C23.3172 9.25332 23.5394 8.06508 22.8896 7.28254C22.2398 6.5 21.031 6.5 18.6133 6.5H4.88917ZM4.88917 6.5L4.33203 3.25"
+                        stroke=""
+                        stroke-width="1.6"
+                        stroke-linecap="round"
+                      />
+                    </svg>
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+        </div>
       </div>
-    </div>
     </>
   );
 };
